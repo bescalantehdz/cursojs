4034b5127393df738f6cc2361a58981f83b05962
Osiris Arce <oao1219@gmail.com>
2019-01-04 17:45:50 -0600

    04/01/2019

---
+++ js/dom.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var caja = document.getElementById("micaja");
+
+caja.innerHTML="cambio de texto desde el JS";
+
+console.log(caja);
---
+++ js/eventos.js
@@ -0,0 +1,3 @@
+'use strict'
+
+var boton = document.querySelector('#boton');
